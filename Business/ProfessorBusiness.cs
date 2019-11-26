928161025e97abfb48fa33399f137bc8937e763e
teste <teste@edu.frei>
2019-11-26 09:49:09 -0300

    jdfg

--- Business/ProfessorBusiness.cs
+++ Business/ProfessorBusiness.cs
@@ -1,5 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+
+
+
 namespace Vivencia19ManhaAPI.Business
 {
+    Database.ProfessorDatabase db = new Database.ProfessorDatabase();
+
+
+
     public class ProfessorBusiness
     {
         

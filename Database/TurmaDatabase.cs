d50ac15d1c9c42d5635834a766c5b6d868a04e8d
teste <teste@edu.frei>
2019-11-26 10:43:41 -0300

    Davi: Database Turma

--- Database/TurmaDatabase.cs
+++ Database/TurmaDatabase.cs
@@ -1,7 +1,29 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
 namespace Vivencia19ManhaAPI.Database
 {
     public class TurmaDatabase
     {
-        
+       Models.db_a5064d_freiContext db = new Models.db_a5064d_freiContext();
+
+        // public void InserirTurma(Models.TbTurma modelo)
+        // {
+        //     db.TbTurma.Add(modelo);
+
+        //     db.SaveChanges();
+        // }
+        // public List<Models.TbTurma> ConsultarTurma()
+        // {
+        //      List<Models.TbTurma> lista = db.TbTurma.ToList();
+        //      return lista;
+        // }
+        // public void AlterarTurma(Models.TbTurma modelo)
+        // {
+        //     Models.TbTurma alterar = db.TbTurma.FirstOrDefault(x=> x.IdTurma == )
+        // }
     }
 }

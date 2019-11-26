731cd47520a4ef1fb613d33c09ad85c04b3c87ed
freivivencia2019 <58184753+freivivencia2019@users.noreply.github.com>
2019-11-26 10:11:40 -0300

    Cristian: comecei o codigo inserir

--- Database/diciplinaDatabase.cs
+++ Database/diciplinaDatabase.cs
@@ -1,7 +1,14 @@
+using System.Linq;
+using System.Linq.Expressions;
 namespace Vivencia19ManhaAPI.Database
 {
     public class diciplinaDatabase
     {
-        
+        public void InserirDisciplina(Models.TbDisciplina disciplina)
+        {
+            Models.
+            db.TbDisciplina.Add(disciplina);
+            db.SaveChanges();
+        }
     }
 }

a6b5a746a3d756209692a178c62f2cdee64ceac2
luanreimberg@gmail.com <luanreimberg@gmail.com>
2019-11-26 14:32:13 -0300

    luan

--- Business/ProfessorBusiness.cs
+++ Business/ProfessorBusiness.cs
@@ -13,22 +13,71 @@ namespace Vivencia19ManhaAPI.Business
 
         public void Inserir(Models.TbProfessor professor)
         {
-            if(professor.NmProfessor == "")
+            if(professor.NmProfessor == string.Empty)
                 throw new Exception("Nome do professor Não pode ser vaziu");
 
-            if(professor.NmMae == "")
+            if(professor.NmMae == string.Empty)
                 throw new Exception("Nome da mãe Não pode ser vaziu");
 
-            if(professor.NmPai == "")
+            if(professor.NmPai == string.Empty)
                 throw new Exception("Nome do pai Não pode ser vaziu");   
 
              if(professor.DsCelular == null)
-                throw new Exception("O Celular Não pode ser vaziu");       
+                throw new Exception("O campo Celular Não pode ser vaziu");       
 
+            if(professor.DsCpf == null)
+                throw new Exception("O campo CPF Não pode ser vaziu");     
 
+            if(professor.DsCurso == string.Empty)
+                throw new Exception("O campo Curso Não pode ser vaziu");     
 
+            // if(professor.DsCvLattes == null)
+            //     throw new Exception("O Celular Não pode ser vaziu");     
 
+            if(professor.DsEmail == string.Empty)
+                throw new Exception("O campo E-mail Não pode ser vaziu");     
 
+            if(professor.DsEstado == string.Empty)
+                throw new Exception("O campo Estado Não pode ser vaziu");     
+
+            if(professor.DsFaculdade == string.Empty)
+                throw new Exception("O campo Faculdade Não pode ser vaziu");     
+
+            if(professor.DsRg == string.Empty)
+                throw new Exception("O campo RG Não pode ser vaziu");     
+
+            if(professor.DsRgEmissor == string.Empty)
+                throw new Exception("O campo RG Emissor Não pode ser vaziu");     
+
+            if(professor.DsRgOrgao == string.Empty)
+                throw new Exception("O campo RG Orgão Não pode ser vaziu");     
+
+            if(professor.DsTelefone == string.Empty)
+                throw new Exception("O campo Telefone Não pode ser vaziu");     
+
+            if(professor.DtFaculdadeFim == null)
+                throw new Exception("O campo Data de Termino da Faculdade Não pode ser vaziu");     
+
+            if(professor.DtFaculdadeInicio == null)
+                throw new Exception("O campo Data de inicio da Faculdade Não pode ser vaziu"); 
+
+            if(professor.DtFaculdadeInicio == DateTime.Now)
+                throw new Exception("O campo inicio da Faculdade Não pode ser igual a data de hoje");         
+
+            if(professor.DtNascimento == DateTime.Now)
+                throw new Exception("O professor Não pode ter nascido agora");     
+
+            if(professor.TpContratacao == string.Empty)
+                throw new Exception("O campo de Contratação Não pode ser vaziu"); 
+
+            if(professor.DsTelefone == string.Empty)
+                throw new Exception("O campo Telefone Não pode ser vaziu");     
+
+            if(professor.DsTelefone == string.Empty)
+                throw new Exception("O campo Telefone Não pode ser vaziu");     
+
+            if(professor.DsTelefone == string.Empty)
+                throw new Exception("O campo Telefone Não pode ser vaziu");     
 
             db.Inserir(professor);
         }
@@ -36,13 +85,81 @@ namespace Vivencia19ManhaAPI.Business
         public void Deletar(int id)
         {
             if (id == 0)
-                throw new ArgumentException("Id inválido");
+                throw new ArgumentException("Id Não informado ou igual a 0");
 
                 db.Deletar(id);
         }
 
         public void Alterar(Models.TbProfessor professor)
         {
+                        if(professor.NmProfessor == string.Empty)
+                throw new Exception("Nome do professor Não pode ser vaziu");
+
+            if(professor.NmMae == string.Empty)
+                throw new Exception("Nome da mãe Não pode ser vaziu");
+
+            if(professor.NmPai == string.Empty)
+                throw new Exception("Nome do pai Não pode ser vaziu");   
+
+             if(professor.DsCelular == null)
+                throw new Exception("O campo Celular Não pode ser vaziu");       
+
+            if(professor.DsCpf == null)
+                throw new Exception("O campo CPF Não pode ser vaziu");     
+
+            if(professor.DsCurso == string.Empty)
+                throw new Exception("O campo Curso Não pode ser vaziu");     
+
+            // if(professor.DsCvLattes == null)
+            //     throw new Exception("O Celular Não pode ser vaziu");     
+
+            if(professor.DsEmail == string.Empty)
+                throw new Exception("O campo E-mail Não pode ser vaziu");     
+
+            if(professor.DsEstado == string.Empty)
+                throw new Exception("O campo Estado Não pode ser vaziu");     
+
+            if(professor.DsFaculdade == string.Empty)
+                throw new Exception("O campo Faculdade Não pode ser vaziu");     
+
+            if(professor.DsRg == string.Empty)
+                throw new Exception("O campo RG Não pode ser vaziu");     
+
+            if(professor.DsRgEmissor == string.Empty)
+                throw new Exception("O campo RG Emissor Não pode ser vaziu");     
+
+            if(professor.DsRgOrgao == string.Empty)
+                throw new Exception("O campo RG Orgão Não pode ser vaziu");     
+
+            if(professor.DsTelefone == string.Empty)
+                throw new Exception("O campo Telefone Não pode ser vaziu");     
+
+            if(professor.DtFaculdadeFim == null)
+                throw new Exception("O campo Data de Termino da Faculdade Não pode ser vaziu");     
+
+            if(professor.DtFaculdadeInicio == null)
+                throw new Exception("O campo Data de inicio da Faculdade Não pode ser vaziu"); 
+
+            if(professor.DtFaculdadeInicio == DateTime.Now)
+                throw new Exception("O campo inicio da Faculdade Não pode ser igual a data de hoje");         
+
+            if(professor.DtNascimento == DateTime.Now)
+                throw new Exception("O professor Não pode ter nascido agora");     
+
+            if(professor.TpContratacao == string.Empty)
+                throw new Exception("O campo de Contratação Não pode ser vaziu"); 
+
+            if(professor.DsTelefone == string.Empty)
+                throw new Exception("O campo Telefone Não pode ser vaziu");     
+
+            if(professor.DsTelefone == string.Empty)
+                throw new Exception("O campo Telefone Não pode ser vaziu");     
+
+            if(professor.DsTelefone == string.Empty)
+                throw new Exception("O campo Telefone Não pode ser vaziu");     
+
+
+
             db.Alterar(professor);
         }
         public List<Models.TbProfessor> ListarTodos()

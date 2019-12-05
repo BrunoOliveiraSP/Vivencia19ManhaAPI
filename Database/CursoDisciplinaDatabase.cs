43d0a1442fdb01c87979449dea6675107a73a1a6
PAC <PAC@NBPAC>
2019-12-05 10:10:08 -0300

    vinicius chaves

--- Database/CursoDisciplinaDatabase.cs
+++ Database/CursoDisciplinaDatabase.cs
@@ -6,8 +6,8 @@ using System.Linq.Expressions;
 
 
 namespace Vivencia19ManhaAPI.Database
-{//a
-    public class CursoDisciplinaDatabase
+{
+        public class CursoDisciplinaDatabase
     {
     Models.db_a5064d_freiContext db = new Models.db_a5064d_freiContext();
 
